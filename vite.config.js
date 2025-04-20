ec45453caa315947db2606c649d7c8000186729a
Mishail <80946997+mikhailhimei@users.noreply.github.com>
2025-04-20 19:28:11 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fookie_instructions/',  // Укажите имя вашего репозитория здесь
 })
